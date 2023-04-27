80cbe7abf1ca72043a6f8be33f2980b9760898a5
allen samuel <allensamuel569@gmail.com>
2023-04-27 17:13:13 +0100

    update

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var navBarShow = false
 const hamburge = document.querySelector('.gpt3__navbar-menu_container');
 
-document.querySelector('.hamburger').addEventListener('click', () =>{
-    if(navBarShow != true){
+document.querySelector('.hamburger').addEventListener('click', () => {
+    if (navBarShow != true) {
         hamburge.style.display = 'block'
         navBarShow = true;
-    }else{
-        hamburge.style.display  = 'none'
+    } else {
+        hamburge.style.display = 'none'
         navBarShow = false;
     }
 })
+
+const hidden = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else if (entry.target.classList.contains('visible')) {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, {
+  threshold: 0.25
+});
+
+hidden.forEach(hide => {
+  observer.observe(hide);
+});
